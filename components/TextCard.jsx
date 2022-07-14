600036db4cb0899f0cd49e7497ab856db3865b57
killerdroid099 <ajempire09@gmail.com>
2022-07-14 23:49:54 +0530

    Added a text-card component

--- components/TextCard.jsx
+++ components/TextCard.jsx
@@ -1,6 +1,6 @@
-function TextCard({ title, desc }) {
+function TextCard({ title, desc, selected }) {
   return (
-    <div className="flex flex-col items-center justify-center border-black border-4 p-6 rounded-xl max-w-[20rem] active:bg-yellow-300 cursor-pointer">
+    <div className={`flex flex-col items-center justify-center border-black border-4 p-6 rounded-xl max-w-[20rem] active:bg-yellow-300 cursor-pointer w-full ${selected && "bg-yellow-300"}`}>
       <h3 className="text-xl font-bold">{title}</h3>
       <p className="text-gray-600 italic">{desc}</p>
     </div>
