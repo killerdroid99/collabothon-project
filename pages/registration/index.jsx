4eb2137a90e7d60522abf655a76c29ebe1a4dcc4
killerdroid099 <ajempire09@gmail.com>
2022-07-15 00:34:27 +0530

    Some additional pages (work in progress)

--- pages/registration/index.jsx
+++ pages/registration/index.jsx
@@ -1,26 +1,33 @@
-import React from 'react'
+import { useState } from 'react'
 import Button from '../../components/Button'
 import TextCard from '../../components/TextCard'
 
 function Index() {
+  const [selected, setSelected] = useState("")
+
   return (
-    <div className=''>
-      <div className='flex justify-center items-center gap-2 mt-28 text-xl'>
-        <strong>Hello, my name is </strong>
-        <input type="text" placeholder='Sue?' className='w-36 px-2 border-[0.185rem] border-black placeholder:text-gray-600 placeholder:font-bold rounded-full text-left bg-inherit outline-none focus-visible:ring-2 ring-yellow-300' />
+    <div className=' px-14'>
+      <div className='flex flex-col gap-2 mt-28 text-base items-center text-left'>
+        <h2 className='text-3xl font-bold'>Hello!</h2>
+        <strong>My name is </strong>
+        <input type="text" placeholder='Sue?' className='w-full max-w-sm px-4 py-4 border-[0.185rem] border-black placeholder:text-gray-600 placeholder:font-bold rounded-3xl text-left bg-inherit outline-none focus-visible:ring-2 ring-accent' />
       </div>
       <div className='my-10 flex flex-col items-center gap-10'>
         <span className='w-[20rem] font-bold text-xl'>I am a...</span>
-
-        <TextCard title="Mentor"
-          desc="Mentors will provide the 
+        <div onClick={() => setSelected("mentor")}>
+          <TextCard title="Mentor"
+            desc="Mentors will provide the 
 roadmap and share resources 
-to facilitate the learning journey." />
-        <TextCard title="Learner"
-          desc="Learners seek to continue 
+to facilitate the learning journey." selected={selected === "mentor"} />
+        </div>
+
+        <div onClick={() => setSelected("learner")}>
+          <TextCard title="Learner"
+            desc="Learners seek to continue 
           learning with the advise and
-          support from mentors"
-        />
+          support from mentors" selected={selected === "learner"}
+          />
+        </div>
         <Button btnText="Let's Go!" />
       </div>
     </div>
