5d6cfe41191884f6c62f0715f5949972145cf27a
killerdroid099 <ajempire09@gmail.com>
2022-07-14 23:50:24 +0530

    Added UI to about page

---
+++ pages/about.jsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+import Button from "../components/Button";
+import image2 from "../public/image2.png"
+
+export default function About() {
+  const router = useRouter()
+
+  return <div>
+    <span className="flex justify-center py-4 text-xl gap-4">Choose Language:
+      <select id="lang" className="bg-inherit border-b-2 border-black w-40 text-center">
+        <option value="english">English</option>
+        <option value="burmese">Burmese</option>
+        <option value="chinese">Chinese</option>
+        <option value="polish">Polish</option>
+      </select>
+    </span>
+
+    <div className="mt-20 mb-5 flex flex-col gap-10 items-center">
+      <p className="text-center w-[24rem] text-xl">For children to continue learning
+        through supportive relationships with
+        mentors who care.
+      </p>
+      <p className="text-center w-[18rem] font-bold">There are 40 million refugee children around the world who have had their educations disrupted. <span className="text-accent">MentorEd</span> pairs students with life-long mentors to continue their learning journey.
+      </p>
+
+      <Button btnText="Continue" handleClick={() => router.push("/features")} />
+    </div>
+
+    <div className="flex flex-col items-center gap-4">
+      <h3 className="text-lg font-bold">Student in need of mentors</h3>
+      <Image src={image2} alt=""></Image>
+      {/* <div className="flex gap-5">
+        <button className="border-[0.2rem] font-bold border-black py-5 px-6 rounded-xl text-left flex flex-col active:bg-yellow-300">
+          <span>Community</span>
+          <span>Forum</span>
+        </button>
+        <button className="border-[0.2rem] font-bold border-black py-5 px-6 rounded-xl text-left flex flex-col active:bg-yellow-300">
+          <span>Resource</span>
+          <span>Library</span>
+        </button>
+      </div> */}
+    </div>
+
+
+  </div>;
+}
