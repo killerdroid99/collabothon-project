fecf8ea3e07b790b1f8a8f669a98b61cc115969e
killerdroid099 <ajempire09@gmail.com>
2022-07-15 09:12:23 +0530

    UI changes

--- pages/registration/index.jsx
+++ pages/registration/index.jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router'
 import { useContext } from 'react'
+import BackToHome from '../../components/BackToHome'
 import Button from '../../components/Button'
 import TextCard from '../../components/TextCard'
 import { UserContext } from "../../context/user-context"
@@ -17,18 +18,20 @@ function Index() {
 
   return (
     <div className=' px-14'>
-      <div className='flex flex-col gap-2 mt-28 text-base items-center text-left'>
+      <BackToHome />
+      <div className='flex flex-col gap-2 mt-28 text-base items-center text-left relative'>
         <h2 className='text-3xl font-bold'>Hello!</h2>
         <strong>My name is </strong>
         <input
           type="text"
           placeholder='Sue?'
-          className='w-full max-w-sm px-4 py-4 border-[0.185rem] border-black placeholder:text-gray-600 placeholder:font-bold rounded-3xl text-left bg-inherit outline-none focus-visible:ring-2 ring-accent'
+          className='w-full max-w-sm px-4 py-4 border-[0.185rem] border-black placeholder:text-gray-600 placeholder:font-bold rounded-3xl text-left bg-inherit outline-none focus-visible:ring-2 ring-accent peer'
           value={state.name}
           onChange={(e) => dispatch({ type: "NAME", payload: e.target.value })}
         />
+        {state.name === "" && <span className='invisible peer-focus-visible:visible text-red-500 absolute -bottom-6'>Name must be atleast 1 character</span>}
       </div>
-      <div className='my-10 flex flex-col items-center gap-10'>
+      <div className='my-10 flex flex-col items-center gap-10 relative'>
         <span className='w-[20rem] font-bold text-xl'>I am a...</span>
         <div onClick={() => dispatch({ type: "MENTOR" })}>
           <TextCard title="Mentor"
@@ -44,6 +47,7 @@ to facilitate the learning journey." selected={state.category === "mentor"} />
           support from mentors" selected={state.category === "learner"}
           />
         </div>
+        {state.category === "" && <span className='absolute top-8 text-red-500'>Select category</span>}
         <Button btnText="Let's Go!" handleClick={handleClick} />
       </div>
     </div>
